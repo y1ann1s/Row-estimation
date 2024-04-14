1c0b361ba9aee65ca2ec929bcdbe70bdb09ee83e
John Meletis <meletisjohn@hotmail.com>
2024-04-14 19:02:01 +0300

    adjust ev variables

---
+++ config.py
@@ -0,0 +1,9 @@
+
+import os
+
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+openai_api_key = os.getenv('OPENAI_API_KEY')
+
+# Other global configuration variables can go here
+prev_wrist_y = None
+
